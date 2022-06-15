f7e613c4a7efb33cefa3ed1a4085a6785d79e6f5
TheLevic <levicrider08@gmail.com>
2022-06-14 19:10:59 -0500

    Added customization

--- userdb.py
+++ userdb.py
@@ -1,24 +1,27 @@
 import sqlite3
 import os
 
+filename = "pwmanager";
+filepath = r"/Users/thelevic/code/personal/password-generator/" + filename;
+
 # Creating the sqlite database
 def createDB():
-    if os.path.exists(r"/Users/thelevic/code/personal/password-generator/pwmanager"):
+    if os.path.exists(filepath):
         print("Database exists. Continuing.");
     else:
-        connection = sqlite3.connect("pwmanager");
+        connection = sqlite3.connect(filepath);
         cursor = connection.cursor();
         cursor.execute("CREATE TABLE userinfo (website TEXT, username TEXT, password TEXT)");
 
 def addToDB(website, username, password):
-    connection = sqlite3.connect("pwmanager");
+    connection = sqlite3.connect(filepath);
     cursor = connection.cursor();
     cursor.execute("insert into userinfo (website, username, password) values (?, ?, ?)",(website, username, password));
     connection.commit();
     connection.close();
 
 def getInfo():
-    connection = sqlite3.connect("pwmanager");
+    connection = sqlite3.connect(filepath);
     cursor = connection.cursor();
     rows = cursor.execute("SELECT website, username, password FROM userinfo").fetchall();
     print(rows);
