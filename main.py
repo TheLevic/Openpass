1bcdef0d6d85993f9ff070671e982d7202605dc2
TheLevic <levicrider08@gmail.com>
2022-06-16 10:42:07 -0500

    Adding error checking

--- main.py
+++ main.py
@@ -22,6 +22,14 @@ class Main():
                 pwlen = input("Invalid input. Please enter a number: ");
         password = pw.generateSecurePassword(pwlen);
         db.addToDB(webname,username,password);
+    def errorStatement(self):
+        print("Sorry. Something went wrong. Please try again or reach out to the developer.");
+    def viewInfo(self):
+        webName = input("Please input the website name (ex: google.com): ");
+        db.getInfo(webName);
+    def deleteInfo(self):
+        webName = input("Please enter website name you would like to delete (ex: google.com): ")
+        db.deleteInfo(webName);
 
     def run(self):
         while (not self.finished):
@@ -41,17 +49,39 @@ class Main():
                 print("Error. Please input a valid number.");
             
             if (self.choice == 1):
-                self.addAccount();
+                try:
+                    self.choice = input("Are you sure you want to add a new account? Y or N: ");
+                    if (self.choice == 'Y' or self.choice == 'y'):
+                        self.addAccount();
+                    else:
+                        print("Returning to home screen.")
+                        pass;
+                except:
+                    self.errorStatement();
+
             elif (self.choice == 2):
-                webName = input("Please input the website name (ex: google.com): ");
-                db.getInfo(webName);
+                try:
+                    self.choice = input("Are you sure you want to view info? Y or N: ");
+                    if (self.choice == 'Y' or self.choice == 'y'):
+                        self.viewInfo();
+                    else:
+                        print("Returning to home screen.")
+                        pass;
+                    
+                except:
+                    self.errorStatement();
+
             elif (self.choice == 3):
-                webName = input("Please enter website name you would like to delete (ex: google.com): ")
-                db.deleteInfo(webName);
+                try:
+                    self.choice = input("Are you sure you want to view info? Y or N: ");
+                    if (self.choice == 'Y' or self.choice == 'y'):
+                        self.deleteInfo();
+                    else:
+                        print("Returning to home screen.")
+                except:
+                    self.errorStatement();
             elif (self.choice == 4):
                 return;
-            else:
-                pass;
 
 main = Main();
 main.run();
